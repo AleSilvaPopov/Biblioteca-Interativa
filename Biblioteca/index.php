ceec76f1bbe7f0f911bf0db721235a875742aaeb
Alexandre da Silva Popov <alexandre.popov406@academico.ufgd.edu.br>
2025-07-09 19:54:15 -0400

    Começando HTML/CSS

---
+++ Biblioteca/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Biblioteca Interativa </title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+
+<body class="bg-blue-950 text-zinc-300">
+        <header class="bg-sky-900">
+            <nav class=" mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+
+                <div class="font-bold text-xl traccking-wide">Biblioteca Interativa</div>
+
+                <ul class="flex space-x-4 font-bold">
+                    <li><a href="/" class="text-emerald-400">Explorar</a></li>
+                    <li><a href="/meus-livros.php" class="hover:underline">Meus Livros</a></li>
+
+                </ul>
+
+                <ul>
+                    <li><a href="/login.php" class="hover:underline">Fazer login</a></li>
+                </ul>
+
+            </nav>
+
+        </header>
+
+        <main>
+
+            Lista de livros
+
+        </main>
+</body>
+</html>
